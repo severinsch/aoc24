9f4c38434ae7a0e24d6f3d0d7f2dbb737ba4ed65
sev <severin.schmidmeier@tum.de>
2024-12-02 16:25:09 +0900

    added day01

---
+++ src/Day01.kt
@@ -0,0 +1,38 @@
+import kotlin.math.abs
+
+fun main() {
+    fun part1(input: List<String>): Int {
+        val left = mutableListOf<Int>()
+        val right = mutableListOf<Int>()
+
+        input.forEach { line ->
+            line
+                .split("   ")
+                .map { it.toInt() }
+                .also { numbers -> left.add(numbers[0]) }
+                .also { numbers -> right.add(numbers[1]) }
+        }
+
+        return left.sorted().zip(right.sorted()).sumOf { (l, r) -> abs(l - r) }
+    }
+
+    fun part2(input: List<String>): Int {
+        val left = mutableListOf<Int>()
+        val appearances = mutableMapOf<Int, Int>()
+
+        input.forEach { line ->
+            line
+                .split("   ")
+                .map { it.toInt() }
+                .also { numbers -> left.add(numbers[0]) }
+                .also { numbers -> appearances.merge(numbers[1], 1) { old, new -> old + new } }
+        }
+
+        return left.sumOf { it * appearances.getOrDefault(it, 0) }
+    }
+
+    // Read the input from the `src/Day01.txt` file.
+    val input = readInput("day01")
+    part1(input).println()
+    part2(input).println()
+}
---
+++ src/Day02.kt
@@ -0,0 +1,15 @@
+
+fun main() {
+    fun part1(input: List<String>): Int {
+
+        return input.size
+    }
+
+    fun part2(input: List<String>): Int {
+        return input.size
+    }
+
+    val input = readInput("day02")
+    part1(input).println()
+    part2(input).println()
+}
