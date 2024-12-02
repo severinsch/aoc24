0fc1fedad31b13bc922ec88b2f69570d02663f1c
sev <severin.schmidmeier@tum.de>
2024-12-02 18:17:23 +0900

    added day02

--- src/Day02.kt
+++ src/Day02.kt
@@ -1,12 +1,36 @@
-
 fun main() {
-    fun part1(input: List<String>): Int {
+    fun checkReport(report: List<Int>): Boolean {
+        val pairs = report.zipWithNext()
+        val increasing = pairs[0].first < pairs[0].second
+        for (pair in pairs) {
+            var diff = pair.first - pair.second
+            if (increasing) {
+                diff *= -1
+            }
+            if ((diff < 1) or (diff > 3)) {
+                return false
+            }
+        }
+        return true
+    }
 
-        return input.size
+    fun part1(input: List<String>): Int {
+        return input.count {
+            line -> line.split(" ").map { it.toInt() }.let { checkReport(it) }
+        }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        fun checkReportWithDampener(report: List<Int>): Boolean {
+            return checkReport(report) or report.indices.any {
+                val newReport = report.toMutableList()
+                newReport.removeAt(it)
+                checkReport(newReport)
+            }
+        }
+        return input.count {
+                line -> line.split(" ").map { it.toInt() }.let { checkReportWithDampener(it) }
+        }
     }
 
     val input = readInput("day02")
