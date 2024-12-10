122d9d8965e2e41d29246955149017555d238ed9
sev <severin.schmidmeier@tum.de>
2024-12-10 14:57:49 +0900

    introduced new helper functions

--- src/Day10.kt
+++ src/Day10.kt
@@ -6,33 +6,25 @@ fun main() {
         val right = Pair(x + 1, y)
         val down = Pair(x, y + 1)
         val left = Pair(x - 1, y)
-        return listOf(up, right, down, left).filter { (a, b) ->
-            a in (0..<input[0].length) && b in input.indices
+        return listOf(up, right, down, left).filter { p ->
+            input.isValidPoint(p)
         }
     }
 
     fun getTrailheads(input: List<String>): List<Pair<Int, Int>> {
-        val trailheads: MutableList<Pair<Int, Int>> = mutableListOf()
-        for (y in input.indices) {
-            for (x in input[0].indices) {
-                if (input[y][x] == '0') {
-                    trailheads.add(Pair(x, y))
-                }
-            }
-        }
-        return trailheads
+        return input.allCoords().filter { input[it] == '0' }
     }
 
     fun part1(input: List<String>): Int {
         val trailheads = getTrailheads(input)
 
         fun getReachableGoals(point: Pair<Int, Int>): Set<Pair<Int, Int>> {
-            val value = input[point.second][point.first].digitToInt()
+            val value = input.getInt(point)
             if (value == 9) {
                 return setOf(point)
             }
-            val surroundingRelevant = getSurroundingCoords(point, input).filter { (x, y) ->
-                (input[y][x].digitToInt()) == value + 1
+            val surroundingRelevant = getSurroundingCoords(point, input).filter { p ->
+                (input.getInt(p)) == value + 1
             }
             return surroundingRelevant.map { getReachableGoals(it) }.reduceOrNull { a, b -> a.union(b) } ?: emptySet()
         }
@@ -44,13 +36,13 @@ fun main() {
         val trailheads = getTrailheads(input)
 
         fun getPaths(point: Pair<Int, Int>, currentPath: List<Pair<Int, Int>>): Set<List<Pair<Int, Int>>> {
-            val value = input[point.second][point.first].digitToInt()
+            val value = input.getInt(point)
             val newPath = currentPath + listOf(point)
             if (value == 9) {
                 return setOf(newPath)
             }
-            val surroundingRelevant = getSurroundingCoords(point, input).filter { (x, y) ->
-                (input[y][x].digitToInt()) == value + 1
+            val surroundingRelevant = getSurroundingCoords(point, input).filter { p ->
+                (input.getInt(p)) == value + 1
             }
             return surroundingRelevant.map { getPaths(it, newPath) }.reduceOrNull { a, b -> a.union(b) } ?: emptySet()
         }
--- src/Utils.kt
+++ src/Utils.kt
@@ -32,3 +32,29 @@ fun executeTimed(f: (List<String>) -> Any, input: List<String>) {
     }
     println("$res, time: $duration")
 }
+
+// some helpful functions to make working with the given input a little easier
+
+typealias Coords = Pair<Int, Int>
+
+operator fun List<String>.get(point: Coords): Char {
+    return this[point.second][point.first]
+}
+
+fun List<String>.getInt(point: Coords): Int {
+    return this[point.second][point.first].digitToInt()
+}
+
+fun List<String>.isValidPoint(point: Coords): Boolean {
+    return point.second in indices && point.first in this[0].indices
+}
+
+fun List<String>.allCoords(): List<Coords> {
+    val res = mutableListOf<Coords>()
+    for (y in this.indices) {
+        for (x in this[0].indices) {
+            res.add(Pair(x, y))
+        }
+    }
+    return res
+}
